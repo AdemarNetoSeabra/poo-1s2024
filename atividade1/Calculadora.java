e22b52881e9808c8682961c63045fcef3c2f73b6
ademarnetoseabra <ademarnetoseabra2234@gmail.com>
2024-03-23 10:47:23 -0300

    atividade1

--- atividade1/Calculadora.java
+++ atividade1/Calculadora.java
@@ -21,28 +21,4 @@ double subtrair(){
 double multiplicar(){
     return numA * numB;
 }
-
-boolean verificarNumeroPrimo(int n){
-
-    if(n <= 1){
-
-        return false;
-    }
-    if( (n !=2 ) && (n % 2 == 0) ){
-        return false;
-    }
-
-    for(int i = 3; i< n; i += 2);
-
-    if(n % 1 == 0){
-
-        return false;
-    }
-
-
-    return true;
-
-
 }
-}
-//oi isso é um comentario//
